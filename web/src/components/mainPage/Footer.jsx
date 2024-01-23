c6ffe1acc38274d4fda473b30b8f66782c30349d
Brbln <a.brbln.97@gmail.com>
2024-01-24 01:58:15 +0300

    Footer düzenlendi.

--- web/src/components/mainPage/Footer.jsx
+++ web/src/components/mainPage/Footer.jsx
@@ -3,30 +3,45 @@ import img from "../images/logo.png"
 import img2 from "../images/logo21.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWhatsapp, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 const Footer = () => {
   return (
-    // <footer style={footerStyle}>
-    //   &copy; 2024 My Website
-    // </footer>
     <>
       <div className="footer">
-        <div className="img container ">
-          <img src={img} alt="" />
+        <img src={img} alt="" />
           {/* <img src={img2} alt="" /> */}
-          <div className="end"> 
-            <div className="icons">
-              <i>
-                <FontAwesomeIcon icon={faFacebook} size="2x" />
-              </i>
-              <i>
-                <FontAwesomeIcon icon={faInstagram} size="2x" />
-              </i>
-              <i>
-                <FontAwesomeIcon icon={faWhatsapp} size="2x" />
-              </i>
-            </div>
-            <p>&copy; 2024 İlkadım Rezonans & Zayıflama</p>
+           <div className="icons">
+            <i>
+              <FontAwesomeIcon icon={faFacebook} size="2x" />
+            </i>
+            <i>
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </i>
+            <i>
+              <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+            </i>
           </div>
+          <div className="footlist">
+            <ul>
+              <li>
+                <Link to='/'>ANASAYFA</Link>
+              </li>
+              <li>
+                <Link to='/hakkimizda/'>HAKKIMIZDA</Link>
+              </li>
+              <li>
+                <Link to='/hizmetlerimiz/'>HİZMETLERİMİZ</Link>
+              </li>
+              <li>
+                <Link to='/Blog/'>BİZDEN GÖRÜNTÜLER</Link>
+              </li>
+              <li>
+                <Link to='/iletisim/'>İLETİŞİM</Link>
+              </li>
+            </ul>
+          </div>
+        <div className="end">
+          <p>&copy; 2024 İlkadım Rezonans & Zayıflama</p>
         </div>
       </div>
     </>
